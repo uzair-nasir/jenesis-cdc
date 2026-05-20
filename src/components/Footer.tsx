e493eb2a04055fd9c5c8ab2c4a881c78bf5f0d4b
Uzair Nasir <108600396+uzair-nasir@users.noreply.github.com>
2026-05-20 09:49:06 -0400

    Fix formatting in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
       <div className="container-edge py-16 grid gap-12 md:grid-cols-12">
         <div className="md:col-span-5">
           <div className="flex items-center gap-3">
-            <img
+            
               src={logo}
               alt="Jenesis CDC"
               width={48}
